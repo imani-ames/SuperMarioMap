7e5343d9d1539bfa34d3e84af4261328c26f1bce
Imani <imaniames3@gmail.com>
2021-03-13 20:54:00 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 34.668243088219484, lng: 135.430204181774 },
+        zoom: 18,
+        mapId: '5fd8cc0b73b40f1f'
+    });
+}
+
+// 34.668243088219484, 135.430204181774
+
